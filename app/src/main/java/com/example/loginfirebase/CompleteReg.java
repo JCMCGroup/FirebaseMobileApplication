3a2ca07321a4dfe01f1c343ef938c43891389082
JCMCGroup-Joshua <121637411+JCMCGroup-Joshua@users.noreply.github.com>
2023-04-28 15:28:41 +0100

    added firestore support

--- app/src/main/java/com/example/loginfirebase/CompleteReg.java
+++ app/src/main/java/com/example/loginfirebase/CompleteReg.java
@@ -4,31 +4,57 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Date;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.AggregateQuery;
+import com.google.firebase.firestore.AggregateQuerySnapshot;
+import com.google.firebase.firestore.AggregateSource;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentChange.Type;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.MetadataChanges;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.Query.Direction;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ServerTimestamp;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Source;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CompleteReg extends Activity {
     TextInputEditText editTextHeight, editTestWeight,DOB;
     Button buttonComReg;
 
-    FirebaseAuth mAuth;
+    FirebaseAuth auth;
 
     ProgressBar progressBar;
-
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +67,9 @@ public class CompleteReg extends Activity {
         editTestWeight = findViewById(R.id.weight);
         buttonComReg = findViewById(R.id.btn_register);
         DOB = findViewById(R.id.dateofbirth);
+        user = auth.getCurrentUser();
         progressBar = findViewById(R.id.progressBar);
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
 //add an onclick listener for the button
@@ -75,29 +102,35 @@ public class CompleteReg extends Activity {
                     Toast.makeText(CompleteReg.this, "Enter Your Date of Birth", Toast.LENGTH_SHORT).show();
                 }
 // making the user
-                FirebaseUser user = mAuth.getCurrentUser();
-                if(user == null){
-                    Intent intent = new Intent(getApplicationContext(),Login.class);
-                    startActivity(intent);
-                    finish();
-                }
-                else{
-                    String UserID = user.getUid();
+                    //Todo: Add SQL Code
+                    Map<String, Object> Height = new HashMap<>();
+                Map<String, Object> Weight = new HashMap<>();
+                Map<String, Object> BMI = new HashMap<>();
+                Map<String, Object> UserBDay = new HashMap<>();
+                    Height.put("User-Height", UserHeight);
+                    Weight.put("User-Weight", UserWeight);
+                    int intUserWeight = Integer.parseInt(UserWeight);
+                    int intUserHeightCM = Integer.parseInt(UserHeight);
+                    double intUserHeightM = intUserHeightCM/100;
+                    double BMICalc = intUserWeight / (intUserHeightM*intUserHeightM);
+                    BMI.put("User-BMI", BMICalc);
+                    UserBDay.put("User-Birthday",UserBirthday);
+                    db.collection("UserDetails").document(user.getEmail()).set(Height);
+                    db.collection("UserDetails").document(user.getEmail()).set(Weight);
+                    db.collection("UserDetails").document(user.getEmail()).set(BMI);
+                    db.collection("UserDetails").document(user.getEmail()).set(UserBDay);
+                if (true){
 
-                }
-                //Todo: Add SQL Code
-                                if (true){
+                                    Intent intent= new Intent(getApplicationContext(),UserHome.class);
 
-                                    Intent intent= new Intent(getApplicationContext(),MainActivity.class);
-
-//start the activity for that call
+                                    //start the activity for that call
 
                                     startActivity(intent);
-//finish the current Activity for that call
+                                    //finish the current Activity for that call
                                     finish();
 
                                 }else {
-// If sign in fails, display a message to the user.
+                                    // If sign in fails, display a message to the user.
 
                                     Toast.makeText(CompleteReg.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
